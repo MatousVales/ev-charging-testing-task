95886be8455f094506470ccc8cec9aca9d76d8bd
Matous Vales <matous.vales@gmail.com>
2025-06-04 20:15:41 +0200

    results card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useCallback, useState} from 'react'
-import {twMerge} from 'tailwind-merge'
 
 import Container from 'components/common/container'
 import Card from 'components/common/card'
@@ -10,12 +9,10 @@ import CardContainer from 'components/common/card-container'
 import Header from 'components/common/header'
 import TopMenu from 'components/common/top-menu'
 import SimulationParameters from 'components/simulation-parameters'
-import Results from 'components/results'
-import Button from 'components/common/button'
+import ResultsCard from 'components/results'
 
 function App() {
 	const configuratorRef = React.createRef<HTMLDivElement>()
-	const resultsRef = React.createRef<HTMLDivElement>()
 	const [resultsStack, setResultsStack] = useState<Array<{id: string}>>([])
 	const [activeResultIndex, setActiveResultIndex] = useState<number>(0)
 	const [isLoading, setIsLoading] = useState(false)
@@ -42,10 +39,7 @@ function App() {
 				setActiveResultIndex(1)
 			}
 		}, 2500)
-		if (resultsRef.current) {
-			resultsRef.current.scrollIntoView({behavior: 'smooth'})
-		}
-	}, [resultsStack, resultsRef])
+	}, [resultsStack])
 
 	const configuartionOverview = (
 		<div>
@@ -62,16 +56,14 @@ function App() {
 					<HeroButton onClick={handleStartConfiguring} label={'Start Configuring'} />
 				</div>
 				<Card title={'Configure Simulation'} ref={configuratorRef}>
-					<div className={'flex flex-row'}>
-						<Grid>
-							<CardContainer title={'Simulation Parameters'}>
-								<SimulationParameters />
-							</CardContainer>
-							<CardContainer title={'Configure Chargers'}>
-								<ChargerConfiguration />
-							</CardContainer>
-						</Grid>
-					</div>
+					<Grid>
+						<CardContainer title={'Simulation Parameters'}>
+							<SimulationParameters />
+						</CardContainer>
+						<CardContainer title={'Configure Chargers'}>
+							<ChargerConfiguration />
+						</CardContainer>
+					</Grid>
 				</Card>
 				<div className={'py-24 sm:py-32 md:py-40'}>
 					<HeroButton
@@ -80,7 +72,7 @@ function App() {
 						label={resultsStack.length >= 1 ? 'Compare Results' : 'Simulate!'}
 					/>
 				</div>
-				<div className={'relative pb-10'} ref={resultsRef}>
+				<div className={'relative pb-10'}>
 					{resultsStack.length > 0 &&
 						resultsStack.map((result, index) => {
 							const isActive = index === activeResultIndex
@@ -88,30 +80,14 @@ function App() {
 							const hasMultipleResults = resultsStack.length > 1
 
 							return (
-								<Card
+								<ResultsCard
 									key={result.id}
 									title={isOriginalResult && hasMultipleResults ? 'Previous Results' : 'Results'}
-									className={twMerge(
-										'min-w-[1200px] relative top-auto left-auto opacity-100 z-20 cursor-default transition-all duration-500 ease-in-out',
-										!isActive &&
-											`absolute top-0 left-0 opacity-60 z-10 cursor-pointer ${
-												isOriginalResult
-													? 'scale-95 translate-x-[-100px] translate-y-[-120px]'
-													: 'scale-95 translate-x-[100px] translate-y-[80px]'
-											}`,
-									)}
 									onClick={() => setActiveResultIndex(index)}
-									detail={
-										<div className={'flex items-center'}>
-											<div className={'mr-10 text-xs'}>{configuartionOverview}</div>
-											<Button onClick={() => {}} className={'px-5 py-2'}>
-												{'Export Results'}
-											</Button>
-										</div>
-									}
-								>
-									<Results />
-								</Card>
+									configuartionOverview={configuartionOverview}
+									isActive={isActive}
+									isOriginalResult={isOriginalResult}
+								/>
 							)
 						})}
 				</div>
--- src/components/results/index.tsx
+++ src/components/results/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import {twMerge} from 'tailwind-merge'
 
+import Card from 'components/common/card'
 import CardContainer from 'components/common/card-container'
 import ChargingValuesChart from 'components/results/charging-values'
 import TotalEnergyChargedChart from 'components/results/total-energy-charged'
@@ -7,10 +9,39 @@ import ChargingEventsChart from 'components/results/charging-events'
 import ExemplaryDayChart from 'components/results/exemplary-day'
 import Grid from 'components/common/grid'
 import HeroButton from 'components/common/hero-button'
+import Button from 'components/common/button'
 
-const Results = () => {
+interface ResultsCardProps {
+	title: string
+	isOriginalResult: boolean
+	isActive: boolean
+	onClick: () => void
+	configuartionOverview: React.ReactNode
+}
+
+const ResultsCard = ({isOriginalResult, isActive, title, configuartionOverview, onClick}: ResultsCardProps) => {
 	return (
-		<div>
+		<Card
+			title={title}
+			className={twMerge(
+				'min-w-[1200px] relative top-auto left-auto opacity-100 z-20 cursor-default transition-all duration-500 ease-in-out',
+				!isActive &&
+					`absolute top-0 left-0 opacity-60 z-10 cursor-pointer ${
+						isOriginalResult
+							? 'scale-95 translate-x-[-100px] translate-y-[-120px]'
+							: 'scale-95 translate-x-[100px] translate-y-[80px]'
+					}`,
+			)}
+			onClick={onClick}
+			detail={
+				<div className={'flex items-center'}>
+					<div className={'mr-10 text-xs'}>{configuartionOverview}</div>
+					<Button onClick={() => {}} className={'px-5 py-2'}>
+						{'Export Results'}
+					</Button>
+				</div>
+			}
+		>
 			<Grid>
 				<CardContainer title={'Average Charger Utilization'}>
 					<ChargingValuesChart />
@@ -32,8 +63,8 @@ const Results = () => {
 			<div className={'flex justify-center mt-6 py-5'}>
 				<HeroButton label={'Get your Free Quote!'} onClick={() => {}} />
 			</div>
-		</div>
+		</Card>
 	)
 }
 
-export default Results
+export default ResultsCard
